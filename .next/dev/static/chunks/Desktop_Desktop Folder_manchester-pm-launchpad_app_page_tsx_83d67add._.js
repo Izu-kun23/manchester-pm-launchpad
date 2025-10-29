ebe4c2e5ba8e5c91fde544355ec0fc7a1562f70e
Izuchukwu Tony <izuchukwuonuoha6@gmail.com>
2025-10-29 16:33:04 +0000

    video implementation

--- .next/dev/static/chunks/Desktop_Desktop Folder_manchester-pm-launchpad_app_page_tsx_83d67add._.js
+++ .next/dev/static/chunks/Desktop_Desktop Folder_manchester-pm-launchpad_app_page_tsx_83d67add._.js
@@ -1,10 +1,10 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/Desktop_Desktop Folder_manchester-pm-launchpad_1548fcd3._.js",
+  "static/chunks/Desktop_Desktop Folder_manchester-pm-launchpad_a8e9b304._.js",
   "static/chunks/10802_motion-dom_dist_es_3962d37b._.js",
   "static/chunks/10802_framer-motion_dist_es_168783a8._.js",
-  "static/chunks/10802_e5cc7fa6._.js"
+  "static/chunks/10802_264bb18f._.js"
 ],
     source: "dynamic"
 });
